ace222b0f202cf992025bc5c1947ad308960524f
Ido Green <greenido@gmail.com>
2014-07-24 12:24:45 +0300

    updating the code to look normal

--- public_html/AppsScript/UI/DatePicker.js
+++ public_html/AppsScript/UI/DatePicker.js
@@ -1,33 +1,24 @@
-function doGet() {
-  var app = UiApp.createApplication();
-  
-  //Create a penel which will hold all the elements
+function showDatesDialog() {
+  var app = UiApp.createApplication();  
   var panel = app.createVerticalPanel();
   
-  //CreateLabels as indicator for dates
-  var dateLabel1 = app.createLabel('Date1');
-  var dateLabel2 = app.createLabel('Date2');
+  var dateLabel1 = app.createLabel('Start Date');
+  var dateLabel2 = app.createLabel('End Date');
   
-  //Create dateboxes for dates  
   var dateBox1 = app.createDateBox().setId('date1');
   var dateBox2 = app.createDateBox().setId('date2');
   
-  //Create a button
   var button = app.createButton('Submit');
   
-  //Create a label which will initially be hidden, it will hold the date 
-  //values which will be visible after submit
   var dateInfo1 = app.createLabel().setId('dateInfo1').setVisible(false);
   var dateInfo2 = app.createLabel().setId('dateInfo2').setVisible(false);
   
-  //createHandler which will show the selected dates.
   var handler = app.createServerClickHandler('showDates');
   handler.addCallbackElement(panel);
   
-  //Add this handler to the button
   button.addClickHandler(handler);
   
-  //Add all the UI elements to the panel
+  // Put all the UI elements on our main panel (like in the good old days of GWT)
   panel.add(dateLabel1)
     .add(dateBox1)
     .add(dateLabel2)
@@ -36,21 +27,20 @@ function doGet() {
     .add(dateInfo1)
     .add(dateInfo2);
   
-  //Add the panel to the application
   app.add(panel);
-  return app; 
+ 
+  var ss = SpreadsheetApp.getActive();
+  // show the UI
+  ss.show(app);
 }
 
-
-//this function will show the dates
-function showDates(e){
-  
-  //get the active aplication
+//
+// Print the dates on the UI
+//
+function showDates(e){  
   var app = UiApp.getActiveApplication();
-  
-  //set the dateInfo labels visible, also make the text as selected dates
+  //  Get the elements by Ids and print the dates
   app.getElementById('dateInfo1').setVisible(true).setText(e.parameter.date1.toString());
   app.getElementById('dateInfo2').setVisible(true).setText(e.parameter.date2.toString());
-  
-  return app;
+
 }
