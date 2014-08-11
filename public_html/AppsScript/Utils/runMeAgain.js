074335b43848373565aaf3433dbe67d9232c9b69
Ido Green <greenido@gmail.com>
2014-08-11 11:49:22 +0300

    initial version

---
+++ public_html/AppsScript/Utils/runMeAgain.js
@@ -0,0 +1,24 @@
+function updateXX() {
+  // Do some work here
+  Logger.log("** Going to work hard & long **");
+  
+  // run me again in 7min = 7*60*1000
+  var tmpTrigger = runMeAgainInXmin(420000, "update92");
+  Logger.log("our trigger for updateXX: " + tmpTrigger.getUniqueId());
+  
+  // Save our triggerId so we can remove it after the execution.
+  // It's also helpful to have all our meta data in a sheet to debug issues.
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName("metaData").getRange(1,1).setValue(tmpTrigger.getUniqueId());
+}
+
+
+//
+// Run me in X milliseconds into the future
+//
+function runMeAgainInXmin(waitForMin, functionToRun) {
+  Logger.log("** Going to set a new trigger for: " + functionToRun +
+   " to run in: " + waitForMin);
+  var oneTimeOnly = ScriptApp.newTrigger(functionToRun).timeBased()
+      .after(waitForMin).create();
+  return oneTimeOnly;
+}
